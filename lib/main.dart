77d8a9f1068b402fb904e2b696effc84db02aa2e
Ibrahim Hatchabi <ibrahim.hatchabiabou@atos.net>
2020-10-17 20:51:05 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:bubble/chatPage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: new Chat(user: 'Ibuss'),
+    );
+  }
+}
